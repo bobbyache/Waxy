7f3f14f58f0ac443ad86ae2a7736012378412c84
Rob Blake <robertb@property24.com>
2017-05-18 14:03:50 +0200

    Creatd SrcImageLocator and a few tests. Some work on landing page url scraper.

---
+++ Code/Prototypes/TcpGrabImage/TcpGrabImage.UnitTests/SrcImageLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CygX1.Waxy.Http.IntegrationTests
+{
+    internal class SrcImageLocator
+    {
+        private string html;
+        private string regularExpression;
+
+        public SrcImageLocator(string html, string regularExpression)
+        {
+            this.regularExpression = regularExpression;
+            this.html = html;
+        }
+
+        internal string GetFirstMatch()
+        {
+            Match match = Regex.Match(html, regularExpression);
+            return match.ToString();
+        }
+    }
+}
---
+++ Code/Prototypes/TcpGrabImage/TcpGrabImage.UnitTests/SrcImageLocatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygX1.Waxy.Http.IntegrationTests
+{
+    [TestFixture]
+    [Category("Image Locator")]
+    class SrcImageLocatorTests
+    {
+        [Test]
+        public void SrcImageLocator_GetFirstMatch_ReturnsCorrectHyperlink_OnPartialFakeLandingPage()
+        {
+            string regEx = @"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)bigbay.jpg&rnd=[0-9]*";
+            string html = TxtFile.ReadText("FakeLandingPage_Partial.txt");
+
+            string expectedSrc = @"https://www.wavescape.co.za/plugins/content/webcam/newfetch.php?pic=bigbay.jpg&rnd=221650030";
+            SrcImageLocator imageLocator = new SrcImageLocator(html, regEx);
+            string hyperlink = imageLocator.GetFirstMatch();
+
+            Assert.AreEqual(expectedSrc, hyperlink);
+        }
+    }
+}
