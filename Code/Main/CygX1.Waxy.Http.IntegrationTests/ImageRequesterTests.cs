b065ff1da6496184e729d66573379c056f15c63c
Rob Blake <robertb@property24.com>
2017-05-19 12:00:51 +0200

    SampleImageRequester moved out of the Http space since it is not a proper configuration.

--- Code/Main/CygX1.Waxy.Http.IntegrationTests/ImageRequesterTests.cs
+++ Code/Main/CygX1.Waxy.Http.IntegrationTests/ImageRequesterTests.cs
@@ -1,7 +1,11 @@
 ﻿using CygX1.Waxy.Http;
 using NUnit.Framework;
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TcpGrabImage.UnitTests
 {
@@ -12,10 +16,52 @@ namespace TcpGrabImage.UnitTests
         [Test]
         public void ImageRequester_DownloadSimpleImage()
         {
-            ImageRequester imageRequester = new ImageRequester();
+            SampleImageRequester imageRequester = new SampleImageRequester();
             Image img = imageRequester.Fetch();
             img.Save(@"C:\Users\robertb\Documents\Work\test.png", ImageFormat.Png);
             Assert.IsNotNull(img);
+        }
+    }
+
+    public class SampleImageRequester
+    {
+        // http://stackoverflow.com/questions/1053052/a-generic-error-occurred-in-gdi-jpeg-image-to-memorystream
+        // http://stackoverflow.com/questions/12022965/adding-http-headers-to-httpclient
+        public Image Fetch()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://www.bouml.fr/doc/figs/diagramstereotypes.png"),
+                Method = HttpMethod.Get,
+                Version = new Version("1.1")
+            };
+
+            request.Headers.Host = "www.bouml.fr";
+            request.Headers.Connection.Add("keep-alive");
+            request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+            request.Headers.Add("Accept", "image/webp,image/*,*/*;q=0.8");
+            request.Headers.Add("Referer", "http://www.bouml.fr/doc/statediagram.html");
+            request.Headers.Add("Accept-Encoding", "gzip, deflate, sdch");
+            request.Headers.Add("Accept-Language", "en-US,en;q=0.8");
+
+            Image img = null;
+
+            var task = client.SendAsync(request)
+                .ContinueWith((taskwithmsg) =>
+                {
+                    var response = taskwithmsg.Result;
+                    Task<byte[]> taskByteArray = response.Content.ReadAsByteArrayAsync();
+                    taskByteArray.Wait();
+
+                    using (MemoryStream memoryStream = new MemoryStream(taskByteArray.Result))
+                    {
+                        img = Image.FromStream(memoryStream);
+                    }
+                });
+            task.Wait();
+
+            return img;
         }
     }
 }
