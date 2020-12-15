4a287e6bf408fb0c33b102977715d3f717cd3298
RobBl8ke <RobB@korbicom.com>
2020-12-15 14:04:58 +0200

    Trying to get unit tests to work (currently the runner doesn't seem to work with .net 5.0

---
+++ Code/Prototypes/PluginBoilerPlate/CygSoft.Waxy.WvScape.Tests/SimpleFileAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace CygSoft.Waxy.WvScape.Tests
+{
+    public class SimpleFileAttribute : DataAttribute
+    {
+        private readonly string _filePath;
+
+        /// <summary>
+        /// Load data from a file as the data source for a theory
+        /// </summary>
+        /// <param name="filePath">The absolute or relative path to the file to load</param>
+        public SimpleFileAttribute(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (testMethod == null) { throw new ArgumentNullException(nameof(testMethod)); }
+
+            // Get the absolute path to the file
+            var path = Path.IsPathRooted(_filePath)
+                ? _filePath
+                : Path.GetRelativePath(Directory.GetCurrentDirectory(), _filePath);
+
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException($"Could not find file at path: {path}");
+            }
+
+            // Load the file
+            var fileData = File.ReadAllText(_filePath);
+
+            //if (string.IsNullOrEmpty(_propertyName))
+            //{
+            //    //whole file is the data
+            //    return JsonConvert.DeserializeObject<List<object[]>>(fileData);
+            //}
+
+            //// Only use the specified property as the data
+            //var allData = JObject.Parse(fileData);
+            //var data = allData[_propertyName];
+            return new List<object[]> { new object[] { fileData } };
+        }
+    }
+}
---
+++ Code/Prototypes/PluginBoilerPlate/CygSoft.Waxy.WvScape.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+using Xunit.Sdk;
+
+namespace CygSoft.Waxy.WvScape.Tests
+{
+    public class Tests
+    {
+        
+        [Theory]
+        [SimpleFile("Sample.txt")]
+        public void Test(string text)
+        {
+            Assert.StartsWith("Name:Kommetjie (WaveScape)", text);
+        }
+
+        [Fact]
+        public void AssertIt()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
